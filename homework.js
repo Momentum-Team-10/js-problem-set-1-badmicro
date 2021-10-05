0c1683fc2a16168795ded2718788e7ec86572dd1
Jeffrey <jrgilley92@gmail.com>
2021-10-05 09:11:18 -0400

    fixed gcd, used recursion :)

--- homework.js
+++ homework.js
@@ -1,23 +1,23 @@
 // 1. Set the variable `givenName` to the string "Addison".
-let givenName = "Addison"
+let givenName = "Addison";
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
-let candies = 20
-let people = 6
-let leftover = (candies%people)
+let candies = 20;
+let people = 6;
+let leftover = candies % people;
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
-function greeting (name) {
-    return "Hello, " + name + "!"
+function greeting(name) {
+  return "Hello, " + name + "!";
 }
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
-function isOdd (num) {
-    if(num<0) num *= -1
-    let bool = num % 2
-    if((bool === 0) || (bool === 1)) return bool
-    return 0
+function isOdd(num) {
+  if (num < 0) num *= -1;
+  let bool = num % 2;
+  if (bool === 0 || bool === 1) return bool;
+  return 0;
 }
 
 // function isOdd (num) {
@@ -31,23 +31,23 @@ function isOdd (num) {
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
-function isEven (num) {
-    if(num<0) num *= -1
-    let bool = num % 2
-    if((bool === 0) || (bool === 1)) return !bool
-    return 0
+function isEven(num) {
+  if (num < 0) num *= -1;
+  let bool = num % 2;
+  if (bool === 0 || bool === 1) return !bool;
+  return 0;
 }
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
-function fahrenheitToCelsius (temp) {
-    return ((temp-32)/1.8)
+function fahrenheitToCelsius(temp) {
+  return (temp - 32) / 1.8;
 }
 // 7. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
-function celsiusToFahrenheit (temp) {
-    return ((temp*1.8)+32)
+function celsiusToFahrenheit(temp) {
+  return temp * 1.8 + 32;
 }
 // 8. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -55,15 +55,15 @@ function celsiusToFahrenheit (temp) {
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
-function fahrenheitToKelvin (temp) {
-    return (fahrenheitToCelsius(temp) + 273.15)
+function fahrenheitToKelvin(temp) {
+  return fahrenheitToCelsius(temp) + 273.15;
 }
 // 9. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
-function lesser (num1, num2) {
-    if(num1 < num2) return num1
-    else return num2
+function lesser(num1, num2) {
+  if (num1 < num2) return num1;
+  else return num2;
 }
 // 10. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -76,16 +76,16 @@ function lesser (num1, num2) {
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
-function multigreeting (name, langCode) {
-    if(langCode === "en") {
-        return "Hello, " + name + "!"
-    } else if (langCode === "es") {
-        return "¡Hola, " + name + "!"
-    } else if (langCode === "fr") {
-        return "Bonjour, " + name + "!"
-    } else if (langCode === "eo") {
-        return "Saluton, " + name + "!"
-    } else return
+function multigreeting(name, langCode) {
+  if (langCode === "en") {
+    return "Hello, " + name + "!";
+  } else if (langCode === "es") {
+    return "¡Hola, " + name + "!";
+  } else if (langCode === "fr") {
+    return "Bonjour, " + name + "!";
+  } else if (langCode === "eo") {
+    return "Saluton, " + name + "!";
+  } else return;
 }
 // 11. The greatest common divisor (https://en.wikipedia.org/wiki/Greatest_common_divisor)
 // is the largest integer that, given two other integers, can be divided into them. For
@@ -113,6 +113,23 @@ function multigreeting (name, langCode) {
 
 // Write a function called gcd that takes two arguments and returns the greatest common
 // divisor using the instructions above.
-function gcd (num1, num2) {
+
+function gcd(num1, num2) {
     
+    if(num1 == num2) {
+        return num1
+    }
+    if(num1 == 0) {
+        return num2
+    }
+    if(num2 == 0) {
+        return num1
+    }
+    
+    if(num1 > num2) {
+        return gcd(num1 - num2, num2)
+    } else {
+        return gcd(num1, num2 - num1)
+    }
+
 }
